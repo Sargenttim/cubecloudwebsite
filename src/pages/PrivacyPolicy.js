e06456d4e854e7eac75dc508b5d4b96d47f0f23b
Timmy <t.cavanagh2k1@gmail.com>
2024-11-20 06:57:04 -0500

    Update PrivacyPolicy.js

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,73 +1,91 @@
-import React from 'react';
+import Link from "next/link";
 
-const APP_NAME = "CubeCloud";
-
-const Welcome = () => {
-    return (
-        <div>
-            <p>Welcome to {appName}!</p>
-            <p>
-            {appName} (“We”, “Us”, or “Our”) is committed to your privacy. This notice describes our Privacy
-            Policy and how we treat data collected on our website and through the services we offer (“Site and
-            Services”) accessible through mobile devices, computers, and tablets. The {appName} Privacy Policy is
-            intended to inform and advise you about how we collect, use, and protect the personal information
-            you provide. By visiting this site, you are accepting the practices described in this Privacy Policy.
-            When we refer to “you” or “your”, we are referring to visitors to the Sites and Services, users of the
-            Sites and Services, and subscribers to the Sites and Services. </p>
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <section className="about-section" id="about-section">
+        <div className="about-header">
+          <h3 className="h-color">Why use this bot?</h3>
         </div>
-    )
-}
-
-
-const WhatWeCollect = () => {
-    return (
-        <div>
-            <h2>WHAT INFORMATION WE COLLECT</h2>
-            <p>Our primary goals in collecting information are to provide and to improve the Site and Services and
-            to administer your use of the Site and Services. Therefore, except as set forth below, we only collect
-            information necessary to provide the services you use, and except as necessary to provide those
-            services, we do not share your personal information with third parties.</p>
-        </div>
-    )
-};
-
-
-const PersonalInformation = () => {
-    return (
-        <div>
-            <h2>Personal Information</h2>
-            <p>
-                Your use of our Sites and Services is voluntary. However, if you wish to use the Sites and Services, we
-                may ask you to provide us with, or grant us access to, or permission to obtain, certain personally
-                identifiable information that can be used to contact or identify you ("Personal Information").
-                Personal Information includes, but is not limited to, your full name, email address, personal photos,
-                the city and state in which you reside, birthdate, the college you attend or attended, and brief
-                biographical information. We use this information to confirm your identity, communicate with you
-                regarding the Site and Services, and provide the Services you have requested. For example, Personal
-                Information is your email address, which you provide to us on our website or when you contact us,
-                and we use to communicate with you. Similarly, your brief biographical information or personal
-                photo may be optionally included in your public {appName} profile.
-            </p>
-            <p>
-                Though our Site offers a payment mechanism for entering Story Contests, all information for
-                processing such payments is stored with our payment processor, Stripe. Please refer to Stripe’s privacy
-                policy to learn about their privacy practices.</p>
-        </div>
-    )
-};
-
-
-class PrivacyPolicy extends React.PureComponent {
-    render(){
-        return (
-            <div style={{ paddingTop: 50 }}>
-                <h1>PRIVACY POLICY</h1>
-                <Welcome />
-                <WhatWeCollect />
+        <div className="about-container">
+          <div className="about-card">
+            <div>
+              <h4 className="h-color">⚡ Commands</h4>
+              <p className="p-color">
+                We have more than 50 commands support in this bot.
+              </p>
+              <hr />
             </div>
-        )
-    }
-};
+            <div>
+              <h4 className="h-color">✉️ 24/7 Support</h4>
+              <p className="p-color">
+                The biggest support community server for the bot available for
+                you anytime.
+              </p>
+              <hr />
+            </div>
+            <div>
+              <h4 className="h-color">✏️ Customization</h4>
+              <p className="p-color">
+                Fully customizable discord bot completely FREE!
+              </p>
+              <hr />
+            </div>
+          </div>
+        </div>
+        <p>
+          <Link href="/commands" className="h-color no-decoration">
+            Check out Commands &rarr;
+          </Link>
+        </p>
 
+        <div className="about-header">
+          <h3 className="h-color">Updates</h3>
+        </div>
+        <div className="about-container">
+          <div className="about-card">
+            <div>
+              <h4 className="h-color">⚡ Bot Updates</h4>
+              <p className="p-color">
+                Lasted Updates:
+                Updated Leveling added new photos
+                Added in Anti System
+              </p>
+              <hr />
+            </div>
+            <div>
+              <h4 className="h-color">✉️ Discord Updates</h4>
+              <p className="p-color">
+                Updated Global Chat system, added new badges for CubeCloud Members and Staff team, Join today to get new roles.
+              </p>
+              <hr />
+            </div>
+          </div>
+        </div>
+        <p>
+          <Link href="https://discord.gg/m4RBuRUuhB" className="h-color no-decoration">
+            Join The Server &rarr;
+          </Link>
+        </p>
+
+
+        <section className="about-stats">
+          <div className="about-stats-card">
+            <h4 className="h-color">60+</h4>
+            <p className="p-color">Total Servers</p>
+          </div>
+          <div className="about-stats-card">
+            <h4 className="h-color">90K+</h4>
+            <p className="p-color">Total Users</p>
+          </div>
+          <div className="about-stats-card">
+            <h4 className="h-color">50+</h4>
+            <p className="p-color">Total Commands</p>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+};
 
 export default PrivacyPolicy;
